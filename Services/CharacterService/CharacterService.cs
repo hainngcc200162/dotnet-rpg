6005078abe48cff65259da441407598ce885da7d
Nguyen Ngoc Hai <98210280+hainngcc200162@users.noreply.github.com>
2023-05-31 18:15:45 +0700

    Fix Update Function

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -85,12 +85,14 @@ namespace dotnet_rpg.Services.CharacterService
                     throw new Exception($"Character with Id'{id}' not found");
                 }
 
-                character.Name = updatedCharacter.Name;
-                character.HitPoints = updatedCharacter.HitPoints;
-                character.Strength = updatedCharacter.Strength;
-                character.Defense = updatedCharacter.Defense;
-                character.Intelligence = updatedCharacter.Intelligence;
-                character.Class = updatedCharacter.Class;
+                // character.Name = updatedCharacter.Name;
+                // character.HitPoints = updatedCharacter.HitPoints;
+                // character.Strength = updatedCharacter.Strength;
+                // character.Defense = updatedCharacter.Defense;
+                // character.Intelligence = updatedCharacter.Intelligence;
+                // character.Class = updatedCharacter.Class;
+
+                _mapper.Map(updatedCharacter, character);
 
                 await _context.SaveChangesAsync();
 
